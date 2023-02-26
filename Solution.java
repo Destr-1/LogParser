afb3ddedc20133883aad13b75d3c6a924e4cb0bf
Destr <destr@mail.ru>
2023-02-26 21:53:53 +0300

    changed Solution.java

--- Solution.java
+++ Solution.java
@@ -13,14 +13,14 @@ public class Solution {
         DateFormat formatter = new SimpleDateFormat("d.M.y H:m:s");
         Date after = formatter.parse("19.03.2016 00:00:00");
         Date before = formatter.parse("29.2.2026 5:4:7");
-//        System.out.println(logParser.execute("get user for date = \"30.01.2014 12:56:22\""));
-//        System.out.println(logParser.execute("get user for event = \"DONE_TASK\""));
-//        System.out.println(logParser.execute("get ip for user = \"Vasya Pupkin\""));
-//        System.out.println(logParser.execute("get user"));
+        System.out.println(logParser.execute("get user for date = \"30.01.2014 12:56:22\""));
+        System.out.println(logParser.execute("get user for event = \"DONE_TASK\""));
+        System.out.println(logParser.execute("get ip for user = \"Vasya Pupkin\""));
+        System.out.println(logParser.execute("get user"));
         System.out.println(logParser.execute(
                 "get ip for event = \"SOLVE_TASK\" and date between \"null\" and \"01.01.2099 23:59:59\""));
 
 
-//        System.out.println(logParser.getDoneTaskUsers(after, before, 48));
+        System.out.println(logParser.getDoneTaskUsers(after, before, 48));
     }
 }
